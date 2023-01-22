2133daa0074f8e6fca6a64790deb4bb139310bfe
Sarka Bohmova <sarka.bohmova@seznam.cz>
2023-01-22 12:57:35 +0100

    hlavička

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -1,4 +1,6 @@
-#projekt_1.py: první projekt do Engeto Online Python Akademie
-#author: Šárka Böhmová
-#email: sarka.bohmova@seznam.cz
-#discord: ShareenBoom#6281
+"""projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Šárka Böhmová
+email: sarka.bohmova@seznam.cz
+discord: ShareenBoom#6281
+"""
