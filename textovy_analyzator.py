5f1fbbb276bec7dec554bd3a5d164325c0f1965c
Sarka Bohmova <sarka.bohmova@seznam.cz>
2023-01-22 16:14:16 +0100

    pocet_slov

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -5,30 +5,30 @@ email: sarka.bohmova@seznam.cz
 discord: ShareenBoom#6281
 """
 TEXTS = ['''
-Situated about 10 miles west of Kemmerer,
-Fossil Butte is a ruggedly impressive
-topographic feature that rises sharply
-some 1000 feet above Twin Creek Valley
-to an elevation of more than 7500 feet
-above sea level. The butte is located just
-north of US 30N and the Union Pacific Railroad,
-which traverse the valley. ''',
-'''At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
+Situated about 10 miles west of Kemmerer, 
+Fossil Butte is a ruggedly impressive 
+topographic feature that rises sharply 
+some 1000 feet above Twin Creek Valley 
+to an elevation of more than 7500 feet 
+above sea level. The butte is located just 
+north of US 30N and the Union Pacific Railroad, 
+which traverse the valley.''',
+'''At the base of Fossil Butte are the bright 
+red, purple, yellow and gray beds of the Wasatch 
+Formation. Eroded portions of these horizontal 
+beds slope gradually upward from the valley floor 
+and steepen abruptly. Overlying them and extending 
+to the top of the butte are the much steeper 
+buff-to-white beds of the Green River Formation, 
 which are about 300 feet thick.''',
-'''The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
+'''The monument contains 8198 acres and protects 
+a portion of the largest deposit of freshwater fish 
+fossils in the world. The richest fossil fish deposits 
+are found in multiple limestone layers, which lie some 
+100 feet below the top of the butte. The fossils 
+represent several varieties of perch, as well as 
+other freshwater genera and herring similar to those 
+in modern oceans. Other fish such as paddlefish, 
 garpike and stingray are also present.'''
 ]
 
@@ -63,12 +63,17 @@ vyber_textu = input("Vyber si cislo textu od 1 do 3: ")
 if int(vyber_textu) not in range(1, 4) or not vyber_textu.isnumeric():
     print("Text s vybranym cislem neexistuje! Ukoncuji program...")
     quit()
+elif vyber_textu == "1":
+    vyber_textu = text_1
+elif vyber_textu == "2":
+    vyber_textu = text_2
 else:
-    print("Vybral jsi text číslo: ", vyber_textu)
+    vyber_textu = text_3
 
-pocet_slov_1 = []
+pocet_slov = []
 
-for slovo in text_1:
-    pocet_slov_1.append(slovo.replace("\n", "").strip(",.!?:"))
-    
-    
+for slovo in vyber_textu:
+    pocet_slov.append(slovo.replace("\n", "").strip(",.!?:"))
+    delka_textu = len(pocet_slov)
+
+print(f"Ve vybranem textu je celkem {delka_textu} slov.")
